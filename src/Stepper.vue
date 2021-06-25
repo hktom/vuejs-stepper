949b2bd5ddb862bf91610ac711279f96b566606f
tom hikari <tomprescott1565@gmail.com>
2021-06-25 07:19:14 +0100

    button_goto_to_complete_the_stepper
    New props added: 
    *options.dispatch : let you dispatch an action in the end of the progression
    *options.router: let you push a path in the end of the progression
    *options.goToText: let you place a custom label on the last button
    *options.dispatch_next : let you place the action stepper_next as a middleware, it has to return a boolean value before to pass to the next stepper. Just pass the name of your actions with options.payload_next as props.

--- src/Stepper.vue
+++ src/Stepper.vue
@@ -28,6 +28,7 @@
         <div class="foot">
             <button v-if="currentPosition > 0" class="prev-button" type="button" @click="prev()">{{options.prevText ? options.prevText : 'Prev' }}</button>
             <button v-if="currentPosition < options.headers.length - 1" class="next-button" type="button" @click="next()">{{options.nextText ? options.nextText : 'Next' }}</button>
+            <button v-if="currentPosition >= options.headers.length - 1" class="next-button goto" type="button" @click="goTo()">{{options.goToText ? options.goToText : 'Go to' }}</button>
         </div>
     </div>
 </template>
@@ -43,10 +44,32 @@
       }
     },
     methods: {
+    goTo(){
+    if(this.options.dispatch){
+     this.$store.dispatch(this.options.dispatch, {data:this.options.payload});
+    }
+    
+    if(this.options.router){
+    this.$router.push(this.options.router);
+    }
+    },
       next () {
         if (this.currentPosition < this.options.headers.length - 1) {
-          this.transitionType = 'stepper-slide-1'
-          this.currentPosition++
+          this.transitionType = 'stepper-slide-1';
+          
+          if(this.options.dispatch_next){ 
+           this.$store.dispatch('stepper_next', {
+           data:this.options.payload_next,
+           currentPosition: this.currentPosition
+           }).then((result)=>{
+             this.currentPosition++
+           });
+     
+          }
+          else
+          {
+              this.currentPosition++
+          }
         }
       },
       prev () {
